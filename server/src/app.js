a3d49eacf39763aeb3cbc88a8d7201e2f97b7be4
asaltcode <elangovanr558@gmail.com>
2024-04-23 08:37:15 +0530

    updated

--- server/src/app.js
+++ server/src/app.js
@@ -11,6 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/v1", AppRouters)
+routers.get('/',(req, res) => res.status(200).send({message: "Api Working Find"}))
 app.use(errorHandle)
 
 
