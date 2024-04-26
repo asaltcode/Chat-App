16ce20aee97485d5076728022a5ffac9db496a68
asaltcode <elangovanr558@gmail.com>
2024-04-26 11:01:10 +0530

    updated

--- socket/index.js
+++ socket/index.js
@@ -1,55 +1,51 @@
-import { Server } from "socket.io";
-import dotenv from 'dotenv'
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import dotenv from 'dotenv';
 
-dotenv.config()
-const PORT = 8800 || process.env.PORT
+dotenv.config();
 
-const io = new Server({
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
   cors: {
     origin: ["http://localhost:5173", "https://chat-app-pgpl.vercel.app"],
-    // origin: "https://chat-app-pgpl.vercel.app",
     methods: ["GET", "POST", "OPTIONS"]
-  },
+  }
 });
 
 let activeUsers = [];
 
+app.get('/', (req, res) => {
+  res.sendFile('E:/test/index.html');
+});
+
 io.on("connection", (socket) => {
-  // add new User
   socket.on("new-user-add", (newUserId) => {
-    // if user is not added previously
     if (!activeUsers.some((user) => user.userId === newUserId)) {
       activeUsers.push({ userId: newUserId, socketId: socket.id });
       console.log("New User Connected", activeUsers);
     }
-    // send all active users to new user
     io.emit("get-users", activeUsers);
   });
 
   socket.on("disconnect", () => {
-    // remove user from active users
-    activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
+    activeUsers = activeUsers.filter((user) => user?.socketId !== socket.id);
     console.log("User Disconnected", activeUsers);
-    // send all active users to all users
     io.emit("get-users", activeUsers);
   });
 
-  // send message to a specific user
   socket.on("send-message", (data) => {
     const { receiverId } = data;
-    const user = activeUsers.find((user) => user.userId === receiverId);
+    const user = activeUsers.find((user) => user?.userId === receiverId);
     console.log("Sending from socket to :", receiverId);
     console.log("Data: ", data);
     if (user) {
-      io.to(user.socketId).emit("receive-message", data); // Corrected typo here
+      io.to(user.socketId).emit("receive-message", data);
     }
   });
 });
 
-io.listen(PORT, () => {
-  console.log(`Socket server running on port ${PORT}`);
-});
-
-io.on("error", (error) => {
-  console.error("Socket server error:", error);
+server.listen(process.env.PORT || 8800, () => {
+  console.log(`Socket server running on port ${process.env.PORT || 8800}`);
 });
