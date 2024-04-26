3a667ae466da0cb982cbe43241cf668d9cedf4c4
asaltcode <elangovanr558@gmail.com>
2024-04-26 11:27:45 +0530

    updated

--- socket/index.js
+++ socket/index.js
@@ -13,8 +13,7 @@ const io = new SocketServer(server, {
   cors: {
     origin: ["http://localhost:5173", "https://chat-app-pgpl.vercel.app"],
     methods: ["GET", "POST", "OPTIONS"]
-  },
-  pingTimeout: 6000 // Increase the ping timeout to 6 seconds
+  }
 });
 
 let activeUsers = [];
