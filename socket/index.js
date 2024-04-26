974a099cb0af081e7033cb95805e3b93ad156c05
asaltcode <elangovanr558@gmail.com>
2024-04-26 11:04:02 +0530

    cors add

--- socket/index.js
+++ socket/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
 import dotenv from 'dotenv';
+import cors from "cors"
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
