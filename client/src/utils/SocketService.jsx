f79d7f0f51073106713865fde40a696e74d34353
asaltcode <elangovanr558@gmail.com>
2024-04-27 13:12:20 +0530

    updated

--- client/src/utils/SocketService.jsx
+++ client/src/utils/SocketService.jsx
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io('http://localhost:8800');
-// const socket = io('https://chat-app-thbf.vercel.app');
+// const socket = io('http://localhost:8800');
+const socket = io('https://chat-app-thbf.vercel.app');
 
 export default socket;
